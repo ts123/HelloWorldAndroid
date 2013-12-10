c354a310ade36ac358a268504fa9f62203fad1dc
ts123 <hukurou@gmail.com>
2013-12-11 08:38:37 +0900

    add build.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,7 @@ fi
 wget http://dl.google.com/android/android-sdk_r22.3-linux.tgz
 tar -zxf android-sdk_r22.3-linux.tgz
 export ANDROID_HOME=$PWD/android-sdk-linux
-export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
+export PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}
 echo y | android update sdk --filter 1,2 --no-ui --force > /dev/null
 echo y | android update sdk --all --no-ui --force --filter $(android list sdk --all | grep -i 'Android 4.0.3' | grep -Po '\d+' | head -n 1) > /dev/null
 echo y | android update sdk --all --no-ui --force --filter $(android list sdk --all | grep -i 'Build-tools, revision 17' | grep -Po '\d+' | head -n 1) > /dev/null
